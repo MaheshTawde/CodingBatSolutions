733481171c766b0cf9c44435f8f978a28ceb46a1
MaheshTawde <85336866+MaheshTawde@users.noreply.github.com>
2021-06-05 10:02:41 +0530

    Create fix45.java

---
+++ java/Array-3/fix45.java
@@ -0,0 +1,40 @@
+	public int[] fix45(int[] nums) {
+
+		List<Integer> positionOf4s = new ArrayList<Integer>();
+		List<Integer> positionOf5s = new ArrayList<Integer>();
+		
+		for(int i=0;i<nums.length;i++) {
+			
+			if(nums[i]==4) {
+			  if(nums[i+1]==5){
+			    continue;
+			  }
+				positionOf4s.add(i);
+				continue;
+			}
+			
+			if(nums[i]==5) {
+			  if(i>0){
+			    if(nums[i-1]==4){
+			    continue;
+			  }
+			  }
+				positionOf5s.add(i);
+				continue;
+			}
+		}
+		
+		for(int i=0;i<positionOf4s.size();i++) {
+			doSwap(nums, positionOf4s.get(i)+1, positionOf5s.get(i));
+		}
+		
+		return nums;
+	}
+
+	public int[] doSwap(int[]nums, int p1, int p2) {
+		int temp = nums[p1];
+		nums[p1]=nums[p2];
+		nums[p2]=temp;
+		return nums;
+	}
+	
